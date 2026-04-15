c8d73bb33d83c796698e9eb8aa68d9795c945349
Yeigalla Bhavana <bhavana268004@gmail.com>
2026-04-15 22:53:22 +0530

    Add files via upload

---
+++ Day 2/Day2.2.py
@@ -0,0 +1,5 @@
+age = 25
+def greet():
+    print("Your age is", age)
+greet()
+
---
+++ Day 2/Day2.3.py
@@ -0,0 +1,7 @@
+age = 25 
+_name = "John" 
+score1 = 88
+
+print("Name:", _name)
+print("Age:", age)
+print("Score:", score1)
